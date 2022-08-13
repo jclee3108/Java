ce3629fb82a9e76e667b488979d2daa38b4c3d4b
jaecheon <jclee3108@naver.com>
2022-08-13 23:27:06 +0900

    add

---
+++ src/coding_116.java
@@ -0,0 +1,95 @@
+
+
+class Solution_116 {
+    public int solution(String s) {
+        
+    	int shot_len = 0;
+    	
+    	for(int i = 1; i<=s.length()/2; i++) {
+    		
+    		if(i == 1) {
+    			shot_len = shotText(i,s).length();
+    		}else {
+    			if(shot_len > shotText(i,s).length()) {
+    				shot_len = shotText(i,s).length();
+    			}
+    		}
+    	}
+//    	
+//    	System.out.println(s.length());
+    	
+//    	for(int i = 1; i< s.length()/2; i++) {
+//    		System.out.println(i);	
+//    		System.out.println(shotText(i,s));
+//    		System.out.println(shotText(i,s).length());
+    	
+//    	}
+    	
+//    	System.out.println(shotText(8,s));
+//    	String result = shotText(1, s); 
+        
+//    	System.out.println(result);
+      
+        
+        int answer = shot_len;
+        return answer;
+    }
+    
+	public String shotText(int n, String s) {
+		
+		String ss = s;
+		int num_cnt = 1;
+        String pre_Text = ss.substring(0,n);
+        String this_Text = ss.substring(n,n*2);
+        String result = "";
+        
+        
+        int len_cnt = ss.length();
+        for(int i =1; i<len_cnt/n; i++){
+        	
+
+        	
+//	        	System.out.println(i);
+        	if (i > 1) {
+        		pre_Text = this_Text;
+        	}
+        	
+        	this_Text = ss.substring(n,n*2);
+        	ss = ss.substring(n);
+        	
+//        	
+//	        	System.out.println("pre : " + pre_Text);
+//	        	System.out.println("this : " + this_Text);
+//	        	System.out.println(s);
+//        	
+        	if (pre_Text.equals(this_Text)) {
+        		num_cnt += 1;
+        		
+        	}else {
+        		result = result + Integer.toString(num_cnt) + pre_Text;
+        		num_cnt = 1;
+        	}
+        	
+            
+        }
+        result = result + Integer.toString(num_cnt) + pre_Text;
+        
+        return result.replace("1", "");
+	
+		
+		
+	}
+}
+
+
+
+public class coding_116{
+	public static void main(String[] args) {
+		String s = "xababcdcdababcdcd";
+		
+		
+		Solution_116 sol = new Solution_116();
+		System.out.println(sol.solution(s));
+		
+	}
+}
---
+++ src/coding_118.java
@@ -0,0 +1,34 @@
+
+
+class Solution_118 {
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for(int i=0; i<arr1[0].length; i++){
+            for(int j=0; j<arr1.length; j++){
+                answer[j][i] = arr1[j][i] + arr2[j][i] ;     
+            }
+            
+        } 
+        
+        return answer;
+    }
+
+}
+
+
+
+public class coding_118 {
+	public static void main(String[] args) {
+		int[][] arr1 = {{1,2},{2,3}} ;
+		int[][] arr2 = {{3,4},{5,6}} ;
+		
+		
+		Solution_118 sol = new Solution_118();
+		System.out.println(sol.solution(arr1, arr2));
+		
+	}
+}
