0c29d15d3a7fd53bdfeeec8845396a25210e604a
jaecheon <jclee3108@naver.com>
2022-08-16 22:48:44 +0900

    add

---
+++ src/coding_108.java
@@ -0,0 +1,27 @@
+import java.util.HashMap; 
+
+class Solution_108 {
+	public int solution(int[] numbers, int target) {
+
+		
+		int result =0 ;
+		
+        return result;
+    }
+}
+
+
+
+public class coding_108 {
+
+	public static void main(String[] args) {
+		Solution_108 sol = new Solution_108();
+		int[] numbers = {3};
+		int target = 3;
+		
+		
+		System.out.println(sol.solution(numbers, target));
+
+	}
+
+}
