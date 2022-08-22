8da2b16ca4784c329fbc2829246e989c7c5bd52a
jaecheon <jclee3108@naver.com>
2022-08-22 16:43:26 +0900

    add

--- src/coding_2002.java
+++ src/coding_2002.java
@@ -1,31 +1,29 @@
 
 
 class Solution_2002 {
-    public String solution(String phone_number) {
+	public String solution(String S) {
         
-    	   
-        int num_cnt = phone_number.length();
-        
-         String Sec = "";
-        
-        for(int i=0; i<num_cnt-4; i++){
-            Sec = Sec + "*";
-        }
-
-        String answer = Sec + phone_number.substring(num_cnt-4);
+		String answer = "";
+		
+		
+		S = S.replace("minus", "-");
+		S = S.replace("plus", "+" );
+		
+		answer = S;
+		
         return answer;
     }
 }
 
 
 
+
 public class coding_2002 {
 	public static void main(String[] args) {
-		String phone_number = "01033334444";
-		
+		String S = "plusminusminusplusminusminusplus";
 		
 		Solution_2002 sol = new Solution_2002();
-		System.out.println(sol.solution(phone_number));
+		System.out.println(sol.solution(S));
 		
 	}
 }
